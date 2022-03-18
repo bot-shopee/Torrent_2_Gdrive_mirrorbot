af6616e3e6f2ff865c4b818ef6f3503226ec0c31
bot-shopee <89763560+bot-shopee@users.noreply.github.com>
2022-03-18 13:04:58 +0700

    Add files via upload

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -378,9 +378,9 @@ class GoogleDriveHelper:
                     msg = self.deletefile(durl)
                     LOGGER.info(f"{msg}")
                     return "your clone has been stopped and cloned data has been deleted!", "cancelled"
-                msg += f'<b> ↳Nama: </b><code>{meta.get("name")}</code>\n\n<b>Size: </b><code>{get_readable_file_size(self.transferred_size)}</code>'
+                msg += f'<b>Nama: </b><code>{meta.get("name")}</code>\n\n<b> ↳Size: </b><code>{get_readable_file_size(self.transferred_size)}</code>'
                 msg += f'\n<b> ↳Type: </b><code>Folder</code>'
-                msg += f'\n<b> ↳Format: </b><code>{mimeType}</code>'
+                msg += f'\n<b> ↳Format: </b><code>{meta.get("mimeType")}</code>'
                 buttons = button_build.ButtonMaker()
                 if SHORTENER is not None and SHORTENER_API is not None:
                     surl = requests.get(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={durl}&format=text').text
@@ -403,7 +403,7 @@ class GoogleDriveHelper:
                     buttons.buildbutton(f"{BUTTON_SIX_NAME}", f"{BUTTON_SIX_URL}")
             else:
                 file = self.copyFile(meta.get('id'), parent_id)
-                msg += f'<b> ↳Nama: </b><code>{file.get("name")}</code>'
+                msg += f'<b>Nama: </b><code>{file.get("name")}</code>'
                 durl = self.__G_DRIVE_BASE_DOWNLOAD_URL.format(file.get("id"))
                 buttons = button_build.ButtonMaker()
                 if SHORTENER is not None and SHORTENER_API is not None:
@@ -416,8 +416,8 @@ class GoogleDriveHelper:
                 except:
                     typeee = 'File' 
                 try:
-                    msg += f'\n<b> ↳Ukuran: </b><code>{get_readable_file_size(int(meta.get("size")))}</code>'
-                    msg += f'\n<b> ↳FOrmat: </b><code>{mimeType}</code>'
+                    msg += f'\n\n<b> ↳Ukuran: </b><code>{get_readable_file_size(int(meta.get("size")))}</code>'
+                    msg += f'\n<b> ↳Format: </b><code>{file.get("mimeType")}</code>'
                 except TypeError:
                     pass
                 if INDEX_URL is not None:
@@ -675,14 +675,15 @@ class GoogleDriveHelper:
             LOGGER.info(f"Counting: {name}")
             if drive_file['mimeType'] == self.__G_DRIVE_DIR_MIME_TYPE:
                 self.gDrive_directory(**drive_file)
-                msg += f'<b> ↳Nama: </b><code>{name}</code>'
+                msg += f'<b>Nama: </b><code>{name}</code>'
                 msg += f'\n\n<b> ↳Ukuran: </b><code>{get_readable_file_size(self.total_bytes)}</code>'
                 msg += f'\n<b> ↳Type: </b><code>Folder</code>'
                 msg += f'\n<b> ↳SubFolders: </b><code>{self.total_folders}</code>'
                 msg += f'\n<b> ↳Files: </b><code>{self.total_files}</code>'
-                msg += f'\n<b> ↳Format: </b><code>{mimeType}</code>'
             else:
-                msg += f'<b> ↳Nama: </b><code>{name}</code>'
+                meta = self.getFileMetadata(file_id)
+                file = self.copyFile(meta.get('id'), parent_id)
+                msg += f'<b>Nama: </b><code>{name}</code>'
                 try:
                     typee = drive_file['mimeType']
                 except:
@@ -691,9 +692,8 @@ class GoogleDriveHelper:
                     self.total_files += 1
                     self.gDrive_file(**drive_file)
                     msg += f'\n\n<b> ↳Ukuran: </b><code>{get_readable_file_size(self.total_bytes)}</code>'
-                    msg += f'\n<b> ↳Type: </b><code>{typee}</code>'
                     msg += f'\n<b> ↳Files: </b><code>{self.total_files}</code>'
-                    msg += f'\n<b> ↳Format: </b><code>{mimeType}</code>'
+                    msg += f'\n<b> ↳Format: </b><code>{file.get("mimeType")}</code>'
                 except TypeError:
                     pass
         except Exception as err:
